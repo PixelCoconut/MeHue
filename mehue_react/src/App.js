1d8abd2af99fe9be92524b0f09bbcea05493ff62
♡ Lena ♡ <9893520@student.swin.edu.au>
2017-08-12 21:44:17 +1000

    Created in ReactJS
    Initialised on Desktop. Will try testing on my Laptop sometime.

---
+++ mehue_react/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>MeHue</h1>
+          <h2>Working In Progress</h2>
+        </div>
+        <p className="App-intro">
+          The body stays here. I hope to do more next Saturday. ~ Lena
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
