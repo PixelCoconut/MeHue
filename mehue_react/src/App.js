d7c3ba564bd21a3006cb3f41bb1b625a49a884eb
Lena122 <9893520@student.swin.edu.au>
2017-08-19 23:59:33 +1000

    Added Table

--- mehue_react/src/App.js
+++ mehue_react/src/App.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>MeHue</h1>
-          <h2>Working In Progress</h2>
+          <h1>MeHue: Vietnamese Dialects</h1>
+          <h2>
+            It is now {new Date().toLocaleTimeString()}
+          </h2>
         </div>
         <p className="App-intro">
-          The body stays here. I hope to do more next Saturday. ~ Lena
+          I will make more progress next Saturday. I spent more time on fixing
+          up my entire Trello board ~ Lena, 19th of August 2017
         </p>
+        <hr />
+
+        <table class="center">
+          <tr class="dialects">
+          <th class="start">English Translation</th>
+            <th class="start">North</th>
+            <th class="start">Central</th>
+            <th class="start">South</th>
+          </tr>
+
+          <tr>
+            <td class="start">This</td>
+            <td>này</td>
+            <td>ni</td>
+            <td>này</td>
+          </tr>
+        </table>
       </div>
     );
   }
