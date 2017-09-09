1d93c9f77455e1e5ecd1ef46efae072a5f0016be
Lena122 <9893520@student.swin.edu.au>
2017-09-09 18:30:20 +1000

    Added React-Compliant Table
    Don't understand why there's no border. That will be my next job.

--- mehue_react/src/App.js
+++ mehue_react/src/App.js
@@ -1,24 +1,49 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
+//Shift + Alt + F to use prettier
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>MeHue: Vietnamese Dialects</h1>
-          <h2>
-            It is now {new Date().toLocaleTimeString()}
-          </h2>
+  
+	render() {
+    
+		//test
+		let words = [
+			{
+				word: 'Test',
+				north: 'Test',
+        central: 'Test',
+        south: 'Test' 
+			},
+			{
+				word: 'Test2',
+				north: 'Test2',
+        central: 'Test2',
+        south: 'Test2' 
+			}
+    ];
+    
+		return (
+			<div className="App">
+				<div className="App-header">
+					<h1>MeHue: Vietnamese Dialects</h1>
+					<h2>It is now {new Date().toLocaleTimeString()}</h2>
+				</div>
+				<p className="App-intro">
+					I will make serious progress next Saturday. I'm taking a break from developing for this week. ~
+					Lena, 2nd of September 2017
+				</p>
+				<hr />
+        <div>
+        <BootstrapTable data={ words }>
+        <TableHeaderColumn width='150' dataField='word' isKey>Word</TableHeaderColumn>
+        <TableHeaderColumn width='150' dataField='north'>North</TableHeaderColumn>
+        <TableHeaderColumn width='150' dataField='central'>Central</TableHeaderColumn>
+        <TableHeaderColumn width='150' dataField='south'>South</TableHeaderColumn>
+        </BootstrapTable>
         </div>
-        <p className="App-intro">
-          I will make serious progress next Saturday. I'm taking a break from developing for this week. ~ Lena, 2nd of September 2017
-        </p>
-        <hr />
-        Notes: The table I added before wasn't compliant with ReactJS. I will add a table with three columns to represent the three parts of Vietnam.
-      </div>
-    );
-  }
+			</div>    
+		);
+	}
 }
-
 export default App;
