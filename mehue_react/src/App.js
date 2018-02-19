e29f7b0b81b7ef5ee24f8fa3fab6057448cc4ccb
Lena122 <9893520@student.swin.edu.au>
2018-02-19 20:06:41 +1100

    Minor Changes
    I'll return to this project soon...

--- mehue_react/src/App.js
+++ mehue_react/src/App.js
@@ -4,25 +4,23 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 //Shift + Alt + F to use prettier
 class App extends React.Component {
-  
 	render() {
-    
 		//test
 		let words = [
 			{
 				word: 'Test',
 				north: 'Test',
-        central: 'Test',
-        south: 'Test' 
+				central: 'Test',
+				south: 'Test'
 			},
 			{
 				word: 'Test2',
 				north: 'Test2',
-        central: 'Test2',
-        south: 'Test2' 
+				central: 'Test2',
+				south: 'Test2'
 			}
-    ];
-    
+		];
+
 		return (
 			<div className="App">
 				<div className="App-header">
@@ -30,19 +28,27 @@ class App extends React.Component {
 					<h2>It is now {new Date().toLocaleTimeString()}</h2>
 				</div>
 				<p className="App-intro">
-					I will make serious progress next Saturday. I'm taking a break from developing for this week. ~
-					Lena, 2nd of September 2017
+					I'm too busy playing Medievil Resurrection at the moment ~
+					Lena, 9th of September 2017
 				</p>
 				<hr />
-        <div>
-        <BootstrapTable data={ words }>
-        <TableHeaderColumn width='150' dataField='word' isKey>Word</TableHeaderColumn>
-        <TableHeaderColumn width='150' dataField='north'>North</TableHeaderColumn>
-        <TableHeaderColumn width='150' dataField='central'>Central</TableHeaderColumn>
-        <TableHeaderColumn width='150' dataField='south'>South</TableHeaderColumn>
-        </BootstrapTable>
-        </div>
-			</div>    
+				<div>
+					<BootstrapTable data={words}>
+						<TableHeaderColumn width="150" dataField="word" isKey>
+							Word
+						</TableHeaderColumn>
+						<TableHeaderColumn width="150" dataField="north">
+							North
+						</TableHeaderColumn>
+						<TableHeaderColumn width="150" dataField="central">
+							Central
+						</TableHeaderColumn>
+						<TableHeaderColumn width="150" dataField="south">
+							South
+						</TableHeaderColumn>
+					</BootstrapTable>
+				</div>
+			</div>
 		);
 	}
 }
